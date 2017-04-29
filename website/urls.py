9dcef3bac4580c6b1bcb11c6b82ebdafec5ebe54
faithNassiwa <faithnassiwa@gmail.com>
2017-04-29 13:21:32 +0300

    first commit

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,6 @@ from website import views as website_views
 
 urlpatterns = [
     url(r'^$', website_views.home, name='home'),
-    url(r'^/policy/$', website_views.policy, name='policy'),
+    url(r'^policy/$', website_views.policy, name='policy'),
 
 ]
